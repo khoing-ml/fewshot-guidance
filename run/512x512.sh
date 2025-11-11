e53ef5392dc8357eafb69e3fa84a8857f09a0a2a
khoing-ml <khoingmh@gmail.com>
2025-11-11 16:34:56 +0700

    add 512

---
+++ run/512x512.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+GUIDANCE=1.0
+SEED=42
+NAME="flux-dev"
+SIZE=512
+NUM_STEPS=50
+PROMPT="a cat" # leave it "" for unconditional generation
+
+python infer_cli.py \
+    --name=$NAME \
+    --prompt=$PROMPT \
+    --width=$SIZE \
+    --height=$SIZE \
+    --num_steps=$NUM_STEPS \
+    --guidance=$GUIDANCE \
+    --seed=$SEED \
+    --device="cuda" \
+    --offload # reduce VRAM usage
+
